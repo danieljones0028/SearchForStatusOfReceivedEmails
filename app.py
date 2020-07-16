6a36b76b8ffb686bac77f81c5a2281bc3df94d5b
Daniel Jones <danieljones028@gmail.com>
2020-07-16 13:40:23 -0300

    retornando descartados e recebidos, sem duplicar informaçao da exibiçao

--- app.py
+++ app.py
@@ -6,25 +6,92 @@
 # periodo que contem no mesmo
 
 import os
+import time
+from default_list import emails
 import file_list
 import read_gzfiles
 
-inicio = raw_input('Coloque o numero incial aqui: ')
+# EXEMPLO DE ARQUIVO zimbra.log.XXX.gz ONDE XXX É O NUMERO DO ARQUIVO
+inicio = raw_input('Coloque o numero incial aqui\n(Exemplo zimbra.log.XXX.gz, voce deve digitar o numero representado pelo XXX\n(NAO É PRECISO POR ZEROS A ESQUERDA)): ')
 
 while(inicio.isdigit()) == False:
-    inicio = raw_input('Coloque o numero incial aqui: ')
+    inicio = raw_input('Coloque o numero incial aqui\n(Exemplo zimbra.log.XXX.gz, voce deve digitar o numero representado pelo XXX\n(NAO É PRECISO POR ZEROS A ESQUERDA)): ')
 else:
     inicio = int(inicio)
     pass
 
-final = raw_input('Coloque o numero final aqui: ')
+final = raw_input('Coloque o numero final aqui\n(Exemplo zimbra.log.XXX.gz, voce deve digitar o numero representado pelo XXX\n(NAO É PRECISO POR ZEROS A ESQUERDA)): ')
 
 while(final.isdigit()) == False:
-    final = raw_input('Coloque o numero final aqui: ')
+    final = raw_input('Coloque o numero final aqui\n(Exemplo zimbra.log.XXX.gz, voce deve digitar o numero representado pelo XXX\n(NAO É PRECISO POR ZEROS A ESQUERDA)): ')
 else:
     final = int(final)
     pass
 
+endereco_email = raw_input(
+    """
+    Escolha o endereço de e-mail para verificação:
+        1 nfe1@nazaria.com.br
+        2 nfe2@nazaria.com.br
+        3 nfe3@nazaria.com.br
+        4 nfe4@nazaria.com.br
+        5 nfe5@nazaria.com.br
+        6 nfe6@nazaria.com.br
+        7 nfe7@nazaria.com.br
+        8 nfe8@nazaria.com.br
+        9 nfe9@nazaria.com.br
+        10 nfe10@nazaria.com.br
+        11 nfe11@nazaria.com.br
+        12 nfe12@nazaria.com.br
+        13 nfe13@nazaria.com.br
+        14 nfe14@nazaria.com.br
+        15 nfe15@nazaria.com.br
+        16 nfe16@nazaria.com.br
+        17 nfe17@nazaria.com.br
+
+Digite o numero correspondente ao endereço: """)
+
+while(endereco_email.isdigit()) == False:
+    endereco_email = raw_input(
+    """
+    Escolha o endereço de e-mail para verificação:
+        1 nfe1@nazaria.com.br
+        2 nfe2@nazaria.com.br
+        3 nfe3@nazaria.com.br
+        4 nfe4@nazaria.com.br
+        5 nfe5@nazaria.com.br
+        6 nfe6@nazaria.com.br
+        7 nfe7@nazaria.com.br
+        8 nfe8@nazaria.com.br
+        9 nfe9@nazaria.com.br
+        10 nfe10@nazaria.com.br
+        11 nfe11@nazaria.com.br
+        12 nfe12@nazaria.com.br
+        13 nfe13@nazaria.com.br
+        14 nfe14@nazaria.com.br
+        15 nfe15@nazaria.com.br
+        16 nfe16@nazaria.com.br
+        17 nfe17@nazaria.com.br
+    
+Digite o numero correspondente ao endereço: """)
+else:
+    endereco_email = int(endereco_email)
+    pass
+
+
 # Emails recebidos e descartados
-read_gzfiles.read_received_to(file_list.list_files(inicio, final))
-read_gzfiles.read_received_from(file_list.list_files(inicio, final))
+t1 = time.time()
+m = emails[endereco_email].replace('from=<', '').replace('>,', '')
+print('')
+print('Email escolhido foi: %s' % m)
+
+read_gzfiles.read_received_to(file_list.list_files(inicio, final), emails[endereco_email])
+
+# read_gzfiles.read_received_from(file_list.list_files(inicio, final), emails[endereco_email])
+
+t2 = time.time()
+print('')
+print '%0.3f ms' % ((t2-t1)*1000.0)
+tempo_decorrido = ((t2-t1)*1000.0/(1000*60))%60
+print(tempo_decorrido)
+print('')
--- read_gzfiles.py
+++ read_gzfiles.py
@@ -5,7 +5,8 @@ import os
 import re
 import gzip
 
-from default_list import emails
+# from default_list import emails
+
 
 # TODO: Criar validação para DEB e RPM
 log_dir = '/var/log/'
@@ -14,10 +15,10 @@ log_dir = '/var/log/'
 # l = ['zimbra.log.122.gz', 'zimbra.log.123.gz', 'zimbra.log.124.gz', 'zimbra.log.125.gz', 'zimbra.log.126.gz']
 
 # TODO criar metodo que selecione ao executar o e-mail que sera verificado
-mail_address = emails[13]
+# mail_address = emails[13]
 # mail_address = emails[-1]
 
-def read_received_to(data_list):
+def read_received_to(data_list, mail_address):
 
     mail_ok = []
     mail_spam = []
@@ -26,6 +27,7 @@ def read_received_to(data_list):
 
     try:
         for file in data_list:
+
             if os.path.exists('%s%s' % (log_dir, file)):
                 with gzip.open('%s%s' % (log_dir, file), 'r') as file_path:
                     # TODO: pegar primeira e ultima linha do arquivo para determinar o periodo que o mesmo tem os registros.
@@ -35,15 +37,36 @@ def read_received_to(data_list):
                         line = item.split(" ")
 
                         for l in line:
+                            # s = re.findall('postfix/lmtp', l)
                             s = re.findall('postfix/lmtp', l)
                             if s:
                                 if mail_address in line:
 # ['Mar', '12', '17:06:21', 'zimbra', 'postfix/lmtp[9577]:', '67B7CE28D8:', 'to=<santil.santos@nazaria.com.br>,', 'relay=zimbra.nazaria.com.br[189.80.247.203]:7025,', 'delay=0.66,', 'delays=0.1/0/0.1/0.46,', 'dsn=2.1.5,', 'status=sent', '(250', '2.1.5', 'Delivery', 'OK)']
                                     if line[-1] == 'OK)':
                                         mail_ok.append(line[-1])
-                                    elif line[-1] == 'spam)':
-                                        mail_spam.append(line[-1])
+                                    # elif line[-1] == 'spam)':
+                                    #     mail_spam.append(line[-1])
+            else:
+                print('fiz + nao tinha nada')
 
+            if os.path.exists('%s%s' % (log_dir, file)):
+                with gzip.open('%s%s' % (log_dir, file), 'r') as file_path:
+                    # TODO: pegar primeira e ultima linha do arquivo para determinar o periodo que o mesmo tem os registros.
+                    file_path = file_path.read()
+
+                    for item in file_path.splitlines():
+                        line = item.split(" ")
+
+                        for l in line:
+                            # s = re.findall('postfix/lmtp', l)
+                            s = re.findall('postfix/smtp', l)
+                            if s:
+                                if mail_address in line:
+# ['Mar', '12', '17:06:21', 'zimbra', 'postfix/lmtp[9577]:', '67B7CE28D8:', 'to=<santil.santos@nazaria.com.br>,', 'relay=zimbra.nazaria.com.br[189.80.247.203]:7025,', 'delay=0.66,', 'delays=0.1/0/0.1/0.46,', 'dsn=2.1.5,', 'status=sent', '(250', '2.1.5', 'Delivery', 'OK)']
+                                    if line[-1] == 'spam)':
+                                        mail_spam.append(line[-1])
+                                    # elif line[-1] == 'spam)':
+                                    #     mail_spam.append(line[-1])
             else:
                 print('fiz + nao tinha nada')
 
@@ -66,13 +89,34 @@ def read_received_to(data_list):
             # Foram recebidos 31 e-mails para a conta: nfe14@nazaria.com.br>.
             m_a = mail_address[4:-1]
             print('')
-            print('Foram descartados %s e-mails para a conta: %s.' % (len(mail_ok), m_a))
+            print('Foram descartados %s e-mail(s) para a conta: %s.' % (len(mail_spam), m_a))
             print('')
 
     except TypeError as e:
         print(e)
 
-def read_received_from(data_list):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def read_received_from(data_list, mail_address):
 
     mail_ok = []
     mail_spam = []
